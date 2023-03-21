a6de1ef5ecdf258da5b81f71ba4ce592d69dba47
Lauren Ashley <la.zaubi@gmail.com>
2023-03-20 21:35:41 -0700

    fetch data and display counts for products and categories on admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASS']
 
   def show
+    @products = Product.all
+    @categories = Category.all
   end
 end
