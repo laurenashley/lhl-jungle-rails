fb836dd4853b7a87cb3008392718da4c0e264ae9
Lauren Ashley <la.zaubi@gmail.com>
2023-03-21 11:20:28 -0700

    add about route, start wiring footer link

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  get "about", to: "about#index"
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -16,6 +18,8 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
+
+  # resources :about, only [:index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
