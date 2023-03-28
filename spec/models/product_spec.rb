5b8aff43d9cc458204c13063fb497ed45ebd3cd7
Lauren Ashley <la.zaubi@gmail.com>
2023-03-28 09:31:18 -0700

    all 5 tests passing

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -11,6 +11,17 @@ RSpec.describe Product, type: :model do
       product.quantity = 4
       expect(product).to be_valid
     end
+
+    it "contains a name" do
+      category = Category.new(name: 'name')
+      product = Product.new
+      product.name = nil
+      product.price_cents = '2399'
+      product.category = category
+      product.quantity = 4
+      expect(product).to_not be_valid
+    end
+
     it "contains a price" do
       category = Category.new(name: 'price')
       product = Product.new
@@ -20,15 +31,24 @@ RSpec.describe Product, type: :model do
       product.quantity = 4
       expect(product).to_not be_valid
     end
-    # it "contains a quantity" do
 
-    # end
-    # it "contains a category" do
+    it "contains a quantity" do
+      category = Category.new(name: 'quantity')
+      product = Product.new
+      product.name = 'test'
+      product.price_cents = '2399'
+      product.category = category
+      product.quantity = nil
+      expect(product).to_not be_valid
+    end
 
-    # end
-    # it "contains a name" do
-    #   # product = Product.new
-    #   expect(JSON.parse(response.body)['name']).to eq('Test Product Full')
-    # end
+    it "contains a category" do
+      product = Product.new
+      product.name = 'test'
+      product.price_cents = '2399'
+      product.category = nil
+      product.quantity = 4
+      expect(product).to_not be_valid
+    end
   end
 end
